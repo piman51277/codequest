c913a74099168bb8c26149ece6e45ddc3c9c41ea
piman51277 <piman.dev@gmail.com>
2023-01-31 17:42:50 -0500

    addiply

---
+++ addiply/index.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+int main(){
+  int numTestCases = 0;
+  std::cin >> numTestCases;
+
+  //handle test cases
+  for (int i = 0; i < numTestCases; i++){
+    int num1, num2;
+    std::cin >> num1 >> num2;
+    int sum = num1 + num2;
+    int product = num1 * num2;
+    std::cout << sum << " " << product << std::endl;
+  }
+}
