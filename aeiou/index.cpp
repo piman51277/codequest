4e339b7edc5e7da9e47a827f211a8daaf24e76cc
piman51277 <piman.dev@gmail.com>
2023-01-31 17:52:48 -0500

    aeiou

---
+++ aeiou/index.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+
+int main(){
+  int numTestCases = 0;
+  std::cin >> numTestCases;
+
+  //get rid of newline
+  std::cin.ignore(1);
+
+
+  //handle test cases
+  for (int i = 0; i < numTestCases; i++){
+    std::string caseString;
+
+    std::getline(std::cin, caseString);
+
+    //iterate over the string
+    int vowels = 0;
+    for (char &ch : caseString)
+    {
+      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+      {
+        vowels++;
+      } 
+    }
+    std::cout << vowels << std::endl;
+  }
+}
